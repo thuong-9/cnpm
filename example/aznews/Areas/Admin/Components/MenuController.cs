d814c654de5fcf2772bcabb0b5938f4acf44da4b
thuong <hoaithuong889904@gmail.com>
2025-10-03 12:36:20 +0700

    th7

---
+++ example/aznews/Areas/Admin/Components/MenuController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using aznews.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace aznews.Areas.Admin.Components
+{
+    [Area("Admin")]
+    public class MenuController : Controller
+    {
+        private readonly DataContext _context;
+        public MenuController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var mnList = _context.Menus.OrderBy(m => m.MenuID).ToList();
+            return View(mnList);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var mn = _context.Menus.Find(id);
+            if (mn == null)
+                return NotFound();
+            return View(mn);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var delMenu = _context.Menus.Find(id);
+            if (delMenu == null)
+                return NotFound();
+            _context.Menus.Remove(delMenu);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Create()
+        {
+            var mnList = (from m in _context.Menus
+                        select new SelectListItem()
+                        {
+                            Text = (m.Levels == 1) ? m.MenuName : "-- " + m.MenuName,
+                            Value = m.MenuID.ToString()
+                        }).ToList();
+            mnList.Insert(0, new SelectListItem()
+            {
+                Text = "--- select ---",
+                Value = "0"
+            });
+            ViewBag.mnList = mnList;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(tblMenu mn)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Menus.Add(mn);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mn);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var mn = _context.Menus.Find(id);
+            if (mn == null)
+                return NotFound();
+
+            var mnList = (from m in _context.Menus
+                        select new SelectListItem()
+                        {
+                            Text = (m.Levels == 1) ? m.MenuName : "-- " + m.MenuName,
+                            Value = m.MenuID.ToString()
+                        }).ToList();
+
+            mnList.Insert(0, new SelectListItem()
+            {
+                Text = "--- select ---",
+                Value = "0"
+            });
+
+            ViewBag.mnList = mnList;
+            return View(mn);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(tblMenu mn)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Menus.Update(mn);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mn);
+        }
+    }
+}
--- example/aznews/obj/Debug/net8.0/aznews.AssemblyInfo.cs
+++ example/aznews/obj/Debug/net8.0/aznews.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aznews")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b6f124deabbaef0993bd599df4c9c5afa69c46a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1775520a362ffaaa8c677bd60a34fa1eb3bc6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aznews")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aznews")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
